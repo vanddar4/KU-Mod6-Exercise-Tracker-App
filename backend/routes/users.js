525050ab083b16cb38765886160919f880501c3c
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2020-10-18 23:29:22 -0600

    cant get sessions to work

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const notifier = require("node-notifier");
-let User = require("../models/user.model");
-let NewRegistration = require("../models/newUserRegistration");
+const User = require("../models/user.model");
+const NewRegistration = require("../models/newUserRegistration");
+const bcrypt = require("bcrypt");
 
 router.route("/").get((req, res) => {
   User.find()
@@ -47,4 +48,25 @@ router.route("/register").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
   res.end();
 });
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  NewRegistration.findOne({ username: username }, (error, user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (error, same) => {
+        if (same) {
+          // req.session.userId = user._id;
+          console.log(req.session);
+          console.log("logged in");
+        } else {
+          console.log("didnt log in");
+        }
+      });
+    } else {
+      console.log("didnt log in");
+    }
+  });
+  res.end();
+});
+
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const expressionSession = require("express-session");
 
 require("dotenv").config();
 
@@ -30,7 +31,17 @@ const usersRouter = require("./routes/users");
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 app.use("/registration", usersRouter);
+app.use("/login", usersRouter);
+app.use(
+  expressionSession({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+module.exports = app;
